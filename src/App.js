93339ebcf89eea64fd04c4b770f445561fd284b2
ErastKoliasa <erastyk28@gmail.com>
2023-03-30 17:36:05 +0300

    deploy

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
 import React from 'react';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import {HashRouter, Route, Routes } from 'react-router-dom';
 import ProductsListContainer from './components/Products/ProductsListContainer';
 import NavBar from './components/NavBar/NavBar';
 import Home from './components/Home/Home';
@@ -10,13 +10,13 @@ function App() {
     return (
       <div>
         <NavBar />
-        <BrowserRouter>
+        <HashRouter>
           <Routes>
             <Route path='/' element={<Home/>} />
             <Route path='*' element={<NotfoundPage/>} />
             <Route path='products' element={<ProductsListContainer/>} />
          </Routes>
-      </BrowserRouter>
+      </HashRouter>
       </div>
     );
   }
